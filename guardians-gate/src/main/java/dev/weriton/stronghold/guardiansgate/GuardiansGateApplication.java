cae6c9d0b9d398ad5adc04615a5f543ec6677191
Weriton <eulcf@hotmail.com>
2026-03-03 15:31:46 -0300

    chore(setup): initialize Witcher Stronghold monorepo with Guardians Gate

---
+++ guardians-gate/src/main/java/dev/weriton/stronghold/guardiansgate/GuardiansGateApplication.java
@@ -0,0 +1,13 @@
+package dev.weriton.stronghold.guardiansgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardiansGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardiansGateApplication.class, args);
+	}
+
+}
---
+++ guardians-gate/src/test/java/dev/weriton/stronghold/guardiansgate/GuardiansGateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.weriton.stronghold.guardiansgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardiansGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
